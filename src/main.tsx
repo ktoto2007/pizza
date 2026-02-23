fbbe648fde72664a909590d98dc93dce668732d4
ktoto2007 <shagako2007@gmail.com>
2026-02-24 00:14:44 +0300

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {Admin} from './pages/adminPage/adminPage.tsx'
+import App from './pages/mainPage/mainPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path='admin' element={<Admin/>}/>
+    </Routes>
+  </BrowserRouter>
 )
---
+++ src/pages/adminPage/adminPage.tsx
@@ -0,0 +1,8 @@
+import { useState } from 'react'
+import './admin.css'
+
+export function Admin() {
+  return (
+    <div>hi</div>
+  )
+}
