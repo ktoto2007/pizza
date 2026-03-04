0176294ddc4100c0b647dbef334e306ee3ae03cf
ktoto2007 <shagako2007@gmail.com>
2026-03-04 09:45:06 +0300

    first commit in this branch

---
+++ src/mock-data.ts
@@ -0,0 +1,33 @@
+export const mock = [{ type: "pizza", name: "Маргарита", prices: [399, 549, 699], url: "src/assets/Pizza (2).svg", isOn: true },
+  { type: "pizza", name: "Пепперони", prices: [449, 599, 749], url: "src/assets/Pizza (2).svg", isOn: true },
+  { type: "pizza", name: "Четыре сыра", prices: [499, 649, 799], url: "src/assets/Pizza (2).svg", isOn: true },
+  { type: "pizza", name: "Гавайская", prices: [469, 619, 769], url: "src/assets/Pizza (2).svg", isOn: true },
+  { type: "pizza", name: "Барбекю с курицей", prices: [529, 679, 829], url: "src/assets/Pizza (2).svg", isOn: true },
+
+  { type: "combo", name: "Комбо для двоих", prices: [1299], url: "/img/combo/for_two.jpg", isOn: true },
+  { type: "combo", name: "Семейное комбо", prices: [1999], url: "/img/combo/family.jpg", isOn: true },
+  { type: "combo", name: "Комбо Пепперони + Кола", prices: [899], url: "/img/combo/pepperoni_cola.jpg", isOn: true },
+  { type: "combo", name: "Вечеринка", prices: [2499], url: "/img/combo/party.jpg", isOn: true },
+
+  { type: "drink", name: "Кола 0.5л", prices: [129], url: "/img/drink/cola05.jpg", isOn: true },
+  { type: "drink", name: "Кола 1л", prices: [189], url: "/img/drink/cola1.jpg", isOn: true },
+  { type: "drink", name: "Фанта 0.5л", prices: [129], url: "/img/drink/fanta05.jpg", isOn: true },
+  { type: "drink", name: "Спрайт 0.5л", prices: [129], url: "/img/drink/sprite05.jpg", isOn: true },
+  { type: "drink", name: "Морс ягодный", prices: [159], url: "/img/drink/mors.jpg", isOn: true },
+
+
+  { type: "dessert", name: "Чизкейк Нью-Йорк", prices: [249], url: "/img/dessert/cheesecake.jpg", isOn: true },
+  { type: "dessert", name: "Шоколадный фондан", prices: [269], url: "/img/dessert/fondant.jpg", isOn: true },
+  { type: "dessert", name: "Тирамису", prices: [259], url: "/img/dessert/tiramisu.jpg", isOn: true },
+  { type: "dessert", name: "Брауни", prices: [199], url: "/img/dessert/brownie.jpg", isOn: true },
+
+  { type: "snack", name: "Картофель фри", prices: [179], url: "/img/snack/fries.jpg", isOn: true },
+  { type: "snack", name: "Картофель по-деревенски", prices: [199], url: "/img/snack/potato.jpg", isOn: true },
+  { type: "snack", name: "Куриные наггетсы", prices: [249], url: "/img/snack/nuggets.jpg", isOn: true },
+  { type: "snack", name: "Куриные крылья BBQ", prices: [299], url: "/img/snack/wings.jpg", isOn: true },
+  { type: "snack", name: "Сырные палочки", prices: [229], url: "/img/snack/cheese_sticks.jpg", isOn: true },
+  { type: "snack", name: "Луковые кольца", prices: [189], url: "/img/snack/onion_rings.jpg", isOn: true },
+  { type: "snack", name: "Чесночные гренки", prices: [169], url: "/img/snack/toast.jpg", isOn: true },
+  { type: "snack", name: "Мини-кальцоне", prices: [279], url: "/img/snack/calzone.jpg", isOn: true },
+  { type: "snack", name: "Кесадилья с курицей", prices: [299], url: "/img/snack/quesadilla.jpg", isOn: true },
+  { type: "snack", name: "Кесадилья с сыром", prices: [279], url: "/img/snack/quesadilla_cheese.jpg", isOn: true }]
