b73beb8c648bd8ff72d716b608ac43cd3b4b8614
ktoto2007 <shagako2007@gmail.com>
2026-03-16 11:56:27 +0300

    started styling modal

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -9,10 +9,23 @@ export const Modal = (props: ModalProps) => {
     return (
         <div style={{display: props.show}} className="modal-container">
             <div className="modal-content">
-                <div>Добавить</div>
+                <div className="modal-header">
+                    <div style={{fontSize: 32}}>Добавить</div>
+                    <img className="closeButton" onClick={props.onCLoseButtonCLick} src="src\assets\close.svg" alt=""/>
+                </div>
                 <div className="input-label">Название</div>
-                
-                <div onClick={props.onCLoseButtonCLick} className="closeButton">Закрыть окно</div>
+                <input className="input" type="text" placeholder="Название"/>
+                <div className="input-label">Категория</div>
+                <select className="category-select">
+                    <option value="">Категория</option>
+                    <option value="pizza">Пицца</option>
+                    <option value="combo">Комбо</option>
+                    <option value="snack">Закуски</option>
+                    <option value="drink">Напитки</option>
+                    <option value="dessert">Десерты</option>
+                </select>
+                <div className="input-label">Цена</div>
+                <input className="input" type="text" placeholder="Цена"/>
             </div>
         </div>
     )
