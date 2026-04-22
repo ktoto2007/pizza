87970dd2a475a1946b73b6f186d0560a1aa878a2
ktoto2007 <shagako2007@gmail.com>
2026-04-22 12:01:31 +0300

    started making main page

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import {Admin} from './pages/adminPage/adminPage.tsx'
-import App from './pages/mainPage/mainPage.tsx'
+import {Main} from './pages/mainPage/mainPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path='admin' element={<Admin/>}/>
+      <Route path='main' element={<Main/>}/>
     </Routes>
   </BrowserRouter>
 )
--- src/pages/mainPage/mainPage.tsx
+++ src/pages/mainPage/mainPage.tsx
@@ -1,31 +1,108 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { useShallow } from 'zustand/react/shallow'
+import { useModal, useProducts, type ProductType } from "../../stores"
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+type NavElementProps = {
+  name: string;
+}
+
+const NavElement = (props: NavElementProps) => {
+  const {setCurrentCategory} = useProducts(useShallow(state => ({
+    setCurrentCategory: state.setCurrentCategory
+  })))
+
+  const [type, setType] = useState('');
+
+  useEffect(() => {
+    switch (props.name) {
+      case 'Пиццы':
+        setType('pizza');
+        break;
+      case 'Комбо':
+        setType('combo');
+        break;
+      case 'Напитки':
+        setType('drink');
+        break;
+      case 'Десерты':
+        setType('dessert');
+        break;
+      case 'Закуски':
+        setType('snack');
+        break;
+      default:
+        setType('');
+    }
+  }, [props.name]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-        </a>
-        <a href="https://react.dev" target="_blank">
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div onClick={e => setCurrentCategory(type)} className='navElement'>{props.name}</div>
   )
 }
 
-export default App
+type ProductProps = {
+  id: string
+}
+
+const Product = (props: ProductProps) => {
+  const {products, setSelectedProduct} = useProducts(useShallow(state => ({
+    products: state.products,
+    setSelectedProduct: state.setSelectedProduct
+  })))
+  let product = products.find((product) => product.id === props.id) as ProductType
+
+  return (
+    <div className='product'>
+      <img className='product-img' src={product.url} alt="" />
+      <div className='product-name'>{product.name}</div>
+      <div className='description'>{product.description}</div>
+      <div className='product-price'>от {product.prices[0]} ₽</div>
+    </div>
+  )
+}
+
+const ProductsList = () => {
+  const {products} = useProducts(useShallow(state => ({
+    products: state.products,
+  })))
+  const categoryOrder = ['pizza', 'combo', 'drink', 'snack', 'dessert']
+  const grouped = categoryOrder.flatMap((category) => {
+    const items = products.filter(p => p.category === category && p.isOn)
+    return [
+      {type: 'header', title: category},
+      ...items.map(item => ({type: 'product', ...item}))
+    ]
+  })
+  const renderContent = () => {
+    
+  }
+  return (
+    <div className='products-container'>
+
+    </div>
+  )
+}
+
+export function Main() {
+  
+  return (
+    <div className='container'>
+      <div className='header'>
+        <img src="src\assets\logo.svg" alt="" />
+        <div className='login-button'>Войти</div>
+      </div>
+      <div className='nav'>
+        <div className='category-select'>
+          <NavElement name='Пиццы'/>
+          <NavElement name='Комбо'/>
+          <NavElement name='Напитки'/>
+          <NavElement name='Закуски'/>
+          <NavElement name='Десерты'/>
+        </div>
+        <div className='cart'>Корзина</div>
+      </div>
+      
+    </div>
+  )
+}
